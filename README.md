3870ca981c36aa86cf56a027611ef53cc1951482
Benjamin Chalke <benjamin.chalke@intygrate.com>
2020-02-18 14:44:37 +1100

    #2752 Created basic structure for PHP example

---
+++ README.md
@@ -0,0 +1,1 @@
+# censorreact-php-example
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>censorREACT PHP Demo</title>
+
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+
+<div id='php-demo-content'>
+  
+  <div id='php-demo-title'>
+    <h1>CensorREACT PHP Demo</h1>
+  </div>
+
+  
+
+</div>
+</body>
+</html>
